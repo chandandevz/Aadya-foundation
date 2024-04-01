fac7e5907fd8e9243408d78962c10ca9d162e28c
Chandan <113888446+chandandevz@users.noreply.github.com>
2024-04-01 09:30:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+/*const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  //Navigation arrows
+   navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});  
+*/
+
+const burgerMenu = document.getElementById("mobile-menu-lines");
+
+let menuToggle = document.querySelector(".menu-list")
+
+
+burgerMenu.addEventListener("click", ()=>{
+    menuToggle.classList.toggle("mobile-menu")
+})
+
+//our gallery section code
+/*const galleryImages = document.querySelectorAll("imgs");
+
+const openBtns = document.querySelectorAll("show-pic");
+
+galleryImages.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        openBtns.forEach((openBtn)=>{
+            openBtn.classList.toggle("open-btn")
+        })
+    })
+})*/
 
